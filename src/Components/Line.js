35bd1a6ab880cc27c6acfa55b5afbd58456ee4dd
mikaek <mikaek@ntnu.stud.no>
2020-09-14 13:09:20 +0200

    Added animation to line

--- src/Components/Line.js
+++ src/Components/Line.js
@@ -2,10 +2,12 @@ import React, { useMemo, useCallback, useState } from "react";
 import * as THREE from "three";
 import { useSpring } from "@react-spring/core";
 import { a } from "@react-spring/three";
+// import { useSpring, animated } from "react-spring";
 import Text from "./Text";
+// import { animated, useSpring } from "react-spring-three";
 
 const Line = (props) => {
-  const [hover, setHover] = useState(true);
+  const [hover, setHover] = useState(0);
   const points = useMemo(
     () => [
       new THREE.Vector3(-10, 0, 0),
@@ -14,33 +16,36 @@ const Line = (props) => {
     ],
     []
   );
-  const onUpdate = useCallback((self) => self.setFromPoints(points), [points]);
 
+  const onUpdate = useCallback((self) => self.setFromPoints(points), [points]);
   const { spring } = useSpring({
     spring: hover,
     config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
   });
+  const scale = spring.to([0, 1], [1, 5]);
 
   return (
-    <>
-      <a.mesh
-        onPointerOver={() => setHover(true)}
-        onPointerOut={() => setHover(false)}
-        opacity={spring.to([0], [1])}
-      >
-        <line position={[0, -2.5, -10]}>
-          <bufferGeometry attach="geometry" onUpdate={onUpdate} />
-          <lineBasicMaterial
-            attach="material"
-            color={"#9c88ff"}
-            linewidth={100}
-            linecap={"round"}
-            linejoin={"round"}
-          />
-        </line>
-      </a.mesh>
-      <Text>Hovered: {hover ? "true" : "false"}</Text>
-    </>
+    <group>
+      <a.group position-y={scale}>
+        <a.mesh
+          onClick={() => setHover(Number(!hover))}
+          onPointerOver={() => setHover(Number(1))}
+          onPointerOut={() => setHover(Number(0))}
+        >
+          <line position={[0, -2.5, -10]}>
+            <bufferGeometry attach="geometry" onUpdate={onUpdate} />
+            <lineBasicMaterial
+              name="line"
+              attach="material"
+              color={"#9c88ff"}
+              linewidth={100}
+              linecap={"round"}
+              linejoin={"round"}
+            />
+          </line>
+        </a.mesh>
+      </a.group>
+    </group>
   );
 };
 
