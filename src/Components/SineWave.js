c53cac9f1583dd029cd1b0b4ba3ab3a576e09937
William Dalheim <williad@stud.ntnu.no>
2020-09-14 13:58:02 +0200

    Sine wave test

--- src/Components/SineWave.js
+++ src/Components/SineWave.js
@@ -1,14 +1,48 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, {
+  useMemo,
+  useRef,
+  useCallback,
+  useState,
+  useEffect,
+} from 'react';
 import * as THREE from 'three';
 
 const SineWave = (props) => {
-  const [box, setBox] = useState(new THREE.BoxGeometry(1, 1, 1));
-  const [material, setMaterial] = useState(
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  );
-  const [cube, setCube] = useState(new THREE.Mesh(box, material));
+  const [timeSteps, setTimeSteps] = useState(new Array(1000).fill(0));
+  const [ySteps, setYSteps] = useState([]);
 
-  return <cube></cube>;
+  const [active, setActive] = useState(0);
+  const activeRef = useRef(active);
+  useEffect(() => {
+    setTimeSteps(timeSteps.map((val, i) => i / 10));
+  }, []);
+  useEffect(() => setYSteps(timeSteps.map((val, i) => Math.sin(val) * 5)), [
+    timeSteps,
+  ]);
+
+  const points = useMemo(
+    () => ySteps.map((val, i) => new THREE.Vector3(timeSteps[i], ySteps[i], 0)),
+    [ySteps]
+  );
+
+  const onUpdate = useCallback((self) => self.setFromPoints(points), [points]);
+
+  console.log(timeSteps);
+  console.log(ySteps);
+  return (
+    <group>
+      <line position={[0, 0, 0]}>
+        <bufferGeometry attach="geometry" onUpdate={onUpdate} />
+        <lineBasicMaterial
+          attach="material"
+          color={'#9c88ff'}
+          linewidth={10}
+          linecap={'round'}
+          linejoin={'round'}
+        />
+      </line>
+    </group>
+  );
 };
 
 export default SineWave;
