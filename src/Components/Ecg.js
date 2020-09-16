0508d0ea058f8ae002f28973ba780f30fdb69361
William Dalheim <williad@stud.ntnu.no>
2020-09-16 12:50:22 +0200

    Some changes to ECG.js

--- src/Components/Ecg.js
+++ src/Components/Ecg.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
-import CameraControls from "./CameraControls";
-import Waves from "./Waves";
-import json from "./data.json";
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import CameraControls from './CameraControls';
+import Waves from './Waves';
+import json from './data.json';
 
 const formatDataToPoints = (data) => {
   let points = [];
   console.log(data);
-  let samples = data["samples"];
+  let samples = data['samples'];
   let samplesKeys = Object.keys(samples);
 
   for (let channel in samplesKeys) {
     let arr = samples[samplesKeys[channel]];
     let channelPoints = [];
-
+    let nPoints = 500;
     for (let i in arr) {
-      if (i > 100) break;
+      if (i > nPoints) break;
 
-      channelPoints.push([i * 5, arr[i], channel * 2]);
+      channelPoints.push([i * 1 - nPoints / 2, arr[i], channel * 5]);
     }
 
     points.push(channelPoints);
@@ -30,7 +30,7 @@ const Ecg = () => {
   let points = formatDataToPoints(json);
 
   return (
-    <Canvas camera={{ position: [-40, 10, 10], fov: 35 }}>
+    <Canvas camera={{ position: [-40, 10, -130], fov: 75 }}>
       <CameraControls />
       <ambientLight />
       <pointLight position={[-10, 10, -10]} castShadow />
