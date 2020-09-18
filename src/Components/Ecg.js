9de33b84c959a5018a6b6383e1e4bbf7e350a6c1
mikaek <mikaek@ntnu.stud.no>
2020-09-18 11:10:59 +0200

    Added animation and dynamic rendering (BETA)

--- src/Components/Ecg.js
+++ src/Components/Ecg.js
@@ -1,19 +1,48 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
-import CameraControls from "./CameraControls";
-import json from "./data.json";
+import React, { useState, useEffect, useRef } from "react";
+import { useFrame } from "react-three-fiber";
 import { formatDataToPoints } from "../Scripts/DataConverter";
-import Label from "./Label";
 import { dataService } from "../Services/DataService";
 import Wave from "./Wave";
+import Label from "./Label";
 
-const Ecg = () => {
+const MAX_POINTS_TO_RENDER = 1000;
+
+const getPoints = (points, time) => {
+  let arr = [];
+
+  points.forEach((channel) => {
+    arr.push(channel.slice(time, MAX_POINTS_TO_RENDER + time));
+  });
+
+  return arr;
+};
+
+const Ecg = (props) => {
+  const [renderPoints, setRenderPoints] = useState([]);
+
   let points = formatDataToPoints(dataService.getJSON());
   let channelNames = dataService.getChannelNamesArray();
   console.log(points[3][parseInt(points[3].length / 2)]);
+
+  const mesh = useRef();
+
+  useFrame(() => {
+    if (props.play) mesh.current.position.x -= 0.1;
+  });
+
+  useEffect(() => {
+    let time = 0;
+    const interval = setInterval(() => {
+      time++;
+      setRenderPoints(getPoints(points, time));
+      // console.log(getPoints(points, time));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
-      {points.map((channel, i) => (
+    <mesh ref={mesh}>
+      {renderPoints.map((channel, i) => (
         <>
           <Label position={channel[parseInt(channel.length / 2)]}>
             {channelNames[i]}
@@ -21,7 +50,7 @@ const Ecg = () => {
           <Wave data={channel} key={channel} />
         </>
       ))}
-    </>
+    </mesh>
   );
 };
 
--- src/Components/Scene.js
+++ src/Components/Scene.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
-import CameraControls from './CameraControls';
-import Ecg from './Ecg';
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import CameraControls from "./CameraControls";
+import Ecg from "./Ecg";
 
 const Scene = (props) => {
   return (
     <Canvas
       camera={{ position: [-40, 10, 10], fov: 55 }}
-      style={{ background: '#324444' }}
+      style={{ background: "#324444" }}
     >
       <CameraControls />
       <ambientLight />
       <pointLight position={[-10, 10, -10]} castShadow />
-      <Ecg />
+      <Ecg play={props.play} />
     </Canvas>
   );
 };
