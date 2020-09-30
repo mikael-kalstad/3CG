aa11bd606d809e794ee0a86f8659c3565932d8d9
William Dalheim <williad@stud.ntnu.no>
2020-09-30 14:52:58 +0200

    attempting gitlab ci

--- src/Components/Annotations.js
+++ src/Components/Annotations.js
@@ -3,7 +3,21 @@ import Text from './Text';
 
 const Annotations = (props) => {
   //console.log('timeprops', props.timeProps);
-  return <></>;
+  return (
+    <>
+      <Text
+        position={[5, 5, 5]}
+        rotateToCamera={false}
+        background={true}
+        backgroundOpacity={0.4}
+        backgroundColor={0x000000}
+        backgroundScaleByText={1.5}
+        textSize={2.4}
+      >
+        Hello!
+      </Text>
+    </>
+  );
 };
 
 export default Annotations;
