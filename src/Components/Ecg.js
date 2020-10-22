867dc2d6e098f16b30295dd15ad3abe8bc3cf340
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2020-10-22 10:58:36 +0200

    Moved Channel label into Wave.js

--- src/Components/Ecg.js
+++ src/Components/Ecg.js
@@ -21,28 +21,14 @@ let channelNames = dataService.getChannelNamesArray();
 
 const Ecg = () => {
   const activeChannels = useChannelStore((state) => state.activeChannels);
-  const setChannel = useChannelStore((state) => state.setChannel);
   console.log('%c [Ecg] is rendering', 'background: #111; color: #ebd31c');
   console.log('%c [Wave(s)] is rendering', 'background: #111; color: #ebd31c');
 
-  const [inspected, setInspected] = useInspectStore((state) => [
-    state.inspected,
-    state.setInspected,
-  ]);
   const toggleOrtoMode = useModeStore((state) => state.toggleOrtoMode);
 
   const { gl, camera } = useThree();
   gl.localClippingEnabled = true;
 
-  const inspectChannel = (channelIndex) => {
-    setInspected(channelIndex);
-    for (let i = 0; i < activeChannels.length; i++) {
-      if (i != channelIndex) {
-        setChannel(i, false);
-      }
-    }
-    toggleOrtoMode();
-  };
   return (
     <Suspense fallback={null}>
       <mesh>
@@ -53,20 +39,6 @@ const Ecg = () => {
             (channel, i) =>
               activeChannels[i] && (
                 <React.Fragment key={i}>
-                  <Text
-                    onClick={() => inspectChannel(i)}
-                    position={channel[0].map((val, i) =>
-                      i == 0 ? val - 6 : val
-                    )}
-                    rotateToCamera={true}
-                    background={true}
-                    backgroundOpacity={0.4}
-                    backgroundColor={0x000000}
-                    backgroundScaleByText={1.5}
-                    textSize={2.4}
-                  >
-                    {channelNames[i]}
-                  </Text>
                   <Wave
                     data={channel}
                     channelName={channelNames[i]}
