7c0daa5caf45d7d5506bd5a8537a46e37d112161
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2020-11-04 11:36:56 +0100

    #51 Different design for zoombar

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -3,14 +3,14 @@ import PlayAndPauseButton from './UI/Buttons/PlayAndPauseBtn';
 import SideDrawer from './UI/SideDrawer/SideDrawer';
 import TimeLine from './UI/TimeLine/TimeLine';
 import MarkBtn from './UI/Buttons/MarkBtn';
-import ZoomBar from './UI/ZoomBar';
+import ZoomBar from './UI/Zooming/ZoomBar';
 import GridBtn from './UI/Buttons/GridBtn';
 import OrtoBtn from './UI/Buttons/OrtoBtn';
 import InspectedText from './UI/InspectedText';
-import StockView from './StockView'
+import StockView from './StockView';
 
 const Layout = (props) => {
-  console.log("%c [Layout] is rendering", "background: #111; color: #ebd31c");
+  console.log('%c [Layout] is rendering', 'background: #111; color: #ebd31c');
   return (
     <>
       <MarkBtn />
---
+++ src/Components/UI/Zooming/ZoomBar.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Slider from '@material-ui/core/Slider';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { useCameraStore } from '../../../Store';
+
+const useStyles = makeStyles({
+  root: {},
+  thumb: {
+    width: '25px',
+    backgroundColor: '#efefef',
+    borderRadius: '2px',
+    left: '7px',
+  },
+  mark: {
+    width: '10px',
+    height: 1,
+    borderRadius: 1,
+    left: '9px',
+  },
+  rail: { opacity: 0.7 },
+});
+
+const marks = [
+  {
+    value: -80,
+  },
+  {
+    value: -60,
+  },
+  {
+    value: -40,
+  },
+  {
+    value: -20,
+  },
+  {
+    value: 0,
+  },
+];
+
+const ZoomBar = () => {
+  const [zoomValue, setZoomValue] = useCameraStore((state) => [
+    state.zoomValue,
+    state.setZoomValue,
+  ]);
+
+  const SliderStyle = {
+    height: '250px',
+    top: '120px',
+    right: '30px',
+    position: 'absolute',
+    color: 'white',
+  };
+
+  const classes = useStyles();
+
+  console.log('%c [ZoomBar] is rendering', 'background: #111; color: #ebd31c');
+  return (
+    <Slider
+      style={SliderStyle}
+      orientation="vertical"
+      classes={classes}
+      min={-80}
+      max={0}
+      step={0.2}
+      aria-labelledby="vertical-slider"
+      onChange={(e, val) => setZoomValue(Math.abs(val))}
+      value={-zoomValue}
+      marks={marks}
+      track={false}
+    />
+  );
+};
+
+export default ZoomBar;
