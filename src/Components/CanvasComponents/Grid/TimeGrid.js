73db325b1398ab47477c49b81166f26b9dcafb8c
henrikwt <henrikwt@stud.ntnu.no>
2020-11-19 21:39:18 +0100

    TimeGrid added

--- src/Components/CanvasComponents/Grid/TimeGrid.js
+++ src/Components/CanvasComponents/Grid/TimeGrid.js
@@ -1,5 +1,13 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { Suspense, useEffect, useMemo, useRef } from 'react';
 import * as THREE from 'three';
+import {
+  useAnnotationStore,
+  useInspectStore,
+  useModeStore,
+  useTimelineOptionsStore,
+  useTimeStore,
+} from "../../../Store";
+import Text from '../../Text';
 import { dataService } from '../../../Services/DataService';
 
 const channelNames = dataService.getChannelNamesArray();
@@ -7,157 +15,86 @@ const channelNames = dataService.getChannelNamesArray();
 
 const TimeGrid = (props) => {
 
-  let color = new THREE.Color('red');
-  let size1 = 10;
-  let size2 = 50;
+  let n = 50
+  // Fetch initial time state
+  const startTime = useTimeStore((state) => state.startTime);
+  const endTime = useTimeStore((state) => state.endTime);
 
-  let distance = 3000;
+  let a = (endTime - startTime) * 5;
 
-  const geometry = new THREE.PlaneBufferGeometry(2, 2, 1, 1);
+  let graphLength = 5;
 
-  const material = new THREE.ShaderMaterial({
+  if (a > 5) {
+    graphLength = Math.ceil(a);
+  }
 
-    side: THREE.DoubleSide,
+  // i < 12
+  // 
+  let xPoints = [];
+  for (let i = 0; i < 2; i++) {
+    let m = i * 110
+    xPoints.push(new THREE.Vector3(0, 0, m));
+    xPoints.push(new THREE.Vector3((n * graphLength), 0, m));
+    xPoints.push(new THREE.Vector3(0, 0, m));
 
-    uniforms: {
-      uSize1: {
-        value: size1
-      },
-      uSize2: {
-        value: size2
-      },
-      uColor: {
-        value: color
-      },
-      uDistance: {
-        value: distance
-      }
-    },
-    transparent: true,
-    vertexShader: `
-           
-           varying vec3 worldPosition;
-		   
-           uniform float uDistance;
-           
-           void main() {
-           
-                vec3 pos = position.xzy * uDistance;
-                pos.xz += cameraPosition.xz;
-                
-                worldPosition = pos;
-                
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-           
-           }
-           `,
+  }
+  let xLinesGeo = new THREE.BufferGeometry().setFromPoints(xPoints);
 
+  let yPoints = [];
+  for (let i = 0; i < ((graphLength) + 1); i++) {
+    let m = i * 50
+    yPoints.push(new THREE.Vector3(m, 0, 0));
+    yPoints.push(new THREE.Vector3(m, 0, (n * 2.2)));
+    yPoints.push(new THREE.Vector3(m, 0, 0));
 
-    fragmentShader: `
-           
-           varying vec3 worldPosition;
-           
-           uniform float uSize1;
-           uniform float uSize2;
-           uniform vec3 uColor;
-           uniform float uDistance;
-            
-            
-            
-            float getGrid(float size) {
-            
-                vec2 r = worldPosition.xz / size;
-                
-                
-                vec2 grid = abs(fract(r - 0.5) - 0.5) / fwidth(r);
-                float line = min(grid.x, grid.y);
-                
-            
-                return 1.0 - min(line, 1.0);
-            }
-            
-           void main() {
-           
-                
-                  float d = 1.0 - min(distance(cameraPosition.xz, worldPosition.xz) / uDistance, 1.0);
-                
-                  float g1 = getGrid(uSize1);
-                  float g2 = getGrid(uSize2);
-                  
-                  
-                  gl_FragColor = vec4(uColor.rgb, mix(g2, g1, g1) * pow(d, 3.0));
-                  gl_FragColor.a = mix(0.5 * gl_FragColor.a, gl_FragColor.a, g2);
-                
-                  if ( gl_FragColor.a <= 0.0 ) discard;
-                
-           
-           }
-           
-           `,
+  }
+  let yLinesGeo = new THREE.BufferGeometry().setFromPoints(yPoints);
 
-    extensions: {
-      derivatives: true
-    }
-
+  /*
+  const material = new THREE.LineDashedMaterial({
+    color: 0xffffff,
+    linewidth: 3,
+    scale: 1,
+    dashSize: 3,
+    gapSize: 3,
   });
-
-  const timeGrid = new THREE.Mesh(geometry, material);
-
-
-  const material2 = new THREE.LineBasicMaterial({
-    color: 0xff0000
+  */
+  const material = new THREE.MeshNormalMaterial({
+    color: 0xff0000,
+    opacity: 0.2,
   });
+  const xLines = new THREE.LineSegments(xLinesGeo, material);
+  const yLines = new THREE.LineSegments(yLinesGeo, material);
+  /*
+  xLines.computeLineDistances();
+  yLines.computeLineDistances();
+  */
+  // Conditional rendering to turn grid on/off
+  const gridMode = useModeStore((state) => state.gridMode);
+  const inspected = useInspectStore((state) => state.inspected);
 
-  const n = 50
+  const position = [0, -100, -55];
+  const scale = [0.8, 1, 1];
 
-  const points = [];
-  points.push(new THREE.Vector3(0, 0, 0));
-  points.push(new THREE.Vector3(n, 0, 0));
-  points.push(new THREE.Vector3(n, n, 0));
-  points.push(new THREE.Vector3(0, n, 0));
-  points.push(new THREE.Vector3(0, 0, 0));
-
-  const geometry2 = new THREE.BufferGeometry().setFromPoints(points);
-
-  const line = new THREE.Line(geometry2, material2);
-
-  const gridHelper = new THREE.GridHelper(110, 11);
+  if (!gridMode || (inspected !== -1)) {
+    return <></>;
+  }
 
   return (
-    /*
-    <mesh
-      visible
-      position={10, 10, 10}
-      geometry={geometry}
-      material={material}
-    />
-    */
-    // Infinite Grid here:
-    /*
-     <primitive
-       object={timeGrid}
-       rotation={[1.57, 0, 0]}
-       position={[0, 0, -56]}
-       scale={[0.8, 1, 1]}
-     />
-     */
-    /*
-    <primitive
-      object={line}
-      //rotation={[1.57, 0, 0]}
-      position={[40, 0, -56]}
-      scale={[0.8, 1, 1]}
-    />
-    */
-    /*
-     <primitive
-       object={gridHelper}
-       //rotation={[1.57, 0, 0]}
-       position={[44, 0, 0]}
-       scale={[0.8, 1, 1]}
-     />
-     */
-    <></>
+    <Suspense fallback={null}>
+      <group >
+        <primitive
+          object={xLines}
+          position={position}
+          scale={scale}
+        />
+        <primitive
+          object={yLines}
+          position={position}
+          scale={scale}
+        />
+      </group>
+    </Suspense>
 
   );
 };
